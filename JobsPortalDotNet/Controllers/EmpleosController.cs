8219e28773e17d1d386c01c8ae961c4ea95d70e8
Carlos Parra <carlos.parra.sbr@gmail.com>
2016-03-01 00:46:49 -0400

    Adding EF and creating Empleos Controller

---
+++ JobsPortalDotNet/Controllers/EmpleosController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobsPortalDotNet.Controllers
+{
+    public class EmpleosController : Controller
+    {
+        private proyecto_empleosEntities1 db = new proyecto_empleosEntities1();
+
+        //
+        // GET: /Empleos/
+
+        public ActionResult Index()
+        {
+            var empleos = db.empleos.Include(e => e.categoria1).Include(e => e.usuario);
+            return View(empleos.ToList());
+        }
+
+        //
+        // GET: /Empleos/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            empleo empleo = db.empleos.Find(id);
+            if (empleo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleo);
+        }
+
+        //
+        // GET: /Empleos/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.categoria = new SelectList(db.categorias, "id", "descripcion");
+            ViewBag.published_by = new SelectList(db.usuarios, "id", "nombre");
+            return View();
+        }
+
+        //
+        // POST: /Empleos/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(empleo empleo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.empleos.Add(empleo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.categoria = new SelectList(db.categorias, "id", "descripcion", empleo.categoria);
+            ViewBag.published_by = new SelectList(db.usuarios, "id", "nombre", empleo.published_by);
+            return View(empleo);
+        }
+
+        //
+        // GET: /Empleos/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            empleo empleo = db.empleos.Find(id);
+            if (empleo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.categoria = new SelectList(db.categorias, "id", "descripcion", empleo.categoria);
+            ViewBag.published_by = new SelectList(db.usuarios, "id", "nombre", empleo.published_by);
+            return View(empleo);
+        }
+
+        //
+        // POST: /Empleos/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(empleo empleo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empleo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.categoria = new SelectList(db.categorias, "id", "descripcion", empleo.categoria);
+            ViewBag.published_by = new SelectList(db.usuarios, "id", "nombre", empleo.published_by);
+            return View(empleo);
+        }
+
+        //
+        // GET: /Empleos/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            empleo empleo = db.empleos.Find(id);
+            if (empleo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleo);
+        }
+
+        //
+        // POST: /Empleos/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            empleo empleo = db.empleos.Find(id);
+            db.empleos.Remove(empleo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
